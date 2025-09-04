67cbc9afa7121bd1926a34941797c6ef498bc251
0xmarq <iit2024264@iiita.ac.in>
2025-09-04 13:52:37 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {  
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
+  cout << std::unitbuf;
+  cerr << std::unitbuf;
+  cout << "$ ";
+  string input;
+  getline(std::cin, input);
 }
